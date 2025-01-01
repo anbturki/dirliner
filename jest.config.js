fcd4cde7389b42f4cf94ce1b73520c3e92dcd8b7
Ali Turki <ali@turkii.me>
2025-01-01 17:57:18 +0800

    feat: finalize project setup

---
+++ jest.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    preset: 'ts-jest',
+    testEnvironment: 'node',
+    testMatch: ['**/test/**/*.test.ts'],
+    collectCoverage: true,
+    coverageDirectory: 'coverage',
+    coveragePathIgnorePatterns: ['/node_modules/', '/test/'],
+  };
+  
